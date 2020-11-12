fb48abd3bffe3de64a6b252db39d2ca0b5f86ebc
ArturArakelyan0411 <artur.araqelyan.2004@gmail.com>
2020-11-12 11:30:01 +0400

    responsive, final commit

--- assets/js/app.js
+++ assets/js/app.js
@@ -11,3 +11,14 @@ homeBullets.forEach(bullet => {
         bullet.classList.add('active__bullet');
     });
 });
+
+/***** burger menu *****/
+
+const burgerMenu = document.querySelector('.header__burgerMenu');
+const burgerMenuBtn = document.querySelector('.header__burgerMenu_btn');
+const nav = document.querySelector('nav');
+
+burgerMenu.addEventListener('click', () => {
+    burgerMenuBtn.classList.toggle('is-active');
+    nav.classList.toggle('navIsOpen');
+});
